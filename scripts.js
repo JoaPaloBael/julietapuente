d1d063c020c298a487aff5a36a881603355f7866
Johanna L. Palo Bael <joa.palobael@gmail.com>
2020-07-11 22:33:48 -0300

    Ya la parte del menu de galeria tiene su dropdown

--- scripts.js
+++ scripts.js
@@ -1,14 +1,1 @@
-jQuery ('document').ready(function($){
-
-var menuBtn = $('.icono-menu'),
-    menu = $ ('nav.navegacion ul');
-
-    menuBtn.click(function(){
-        if(menu.hasClass('mostrate') ){
-            menu.removeClass ('mostrate');
-        } else {
-            menu.addClass('mostrate');
-        }
-    });
-
-});
+    $('.collapse').collapse()
