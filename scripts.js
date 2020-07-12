ee55d6695e5a0d900cb6e2aed4fd026b3bfa6818
Johanna L. Palo Bael <joa.palobael@gmail.com>
2020-07-11 21:21:12 -0300

    Hasta aca casi hago el collapse.. veremos que pasa ahora que la flasheo con las clases

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
-$('.carousel').carousel()
-$('.collapse').collapse()
+jQuery ('document').ready(function($){
+
+var menuBtn = $('.icono-menu'),
+    menu = $ ('nav.navegacion ul');
+
+    menuBtn.click(function(){
+        if(menu.hasClass('mostrate') ){
+            menu.removeClass ('mostrate');
+        } else {
+            menu.addClass('mostrate');
+        }
+    });
+
+});
